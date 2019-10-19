61ca96a4b4a9e2de44be617db20000583e4cea9c
Tim Quelch <tim@quelch.name>
2019-10-19 15:57:36 +1000

    Extend interface for primative and add derived sphere

--- src/primative.h
+++ src/primative.h
@@ -3,14 +3,38 @@
 #include <optional>
 
 #include "intersection.h"
+#include "material.h"
 
 namespace renderer {
     class Primative {
+    private:
+        Material material_;
 
     public:
+        Primative(Material material)
+            : material_{std::move(material)} {}
+
         virtual ~Primative() = default;
 
         [[nodiscard]] virtual auto intersect(Ray const& ray) const noexcept
-            -> std::optional<Intersection>;
+            -> std::optional<Intersection> = 0;
+    };
+
+    class Sphere : Primative {
+    private:
+        Vec center_;
+        double radius_;
+
+    public:
+        Sphere(Vec center, double radius, Material material)
+            : Primative{std::move(material)}
+            , center_{std::move(center)}
+            , radius_{radius} {}
+
+        [[nodiscard]] auto center() const noexcept { return center_; }
+        [[nodiscard]] auto radius() const noexcept { return radius_; }
+
+        [[nodiscard]] auto intersect(Ray const& ray) const noexcept
+            -> std::optional<Intersection> override;
     };
 } // namespace renderer
