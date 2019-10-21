2dbd39506bfc0bb928929fbc9be10003826fbb0b
Tim Quelch <tim@quelch.name>
2019-10-21 23:21:48 +1000

    Update test scene

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,21 @@ using namespace renderer;
 
 int main() {
     auto rng = std::mt19937{std::random_device{}()};
-    auto scene = Scene{{0.9, 0.9, 0.9}};
-    auto camera = Camera{{0, -1.5, -8}, {0, 0, 0}, {0, 1, 0}, 40.0, 858, 480};
+    auto const camera = Camera{{0, 3, -8}, {0, 0, 0}, {0, 1, 0}, 40.0, 858, 480};
 
-    // scene.addSphere({0, -1000, -600}, 1000.0, {{1, 1, 1}, {0.0, 0.0, 0.0}});
-    // scene.addSphere({0, 0, 0}, 1.0, {{0, 0, 0}, {0.9, 0.9, 0.9}});
-    // scene.addSphere({2.0, 0.0, -0.3}, 1.0, {{0, 0, 0}, {0.3, 0.3, 0.8}});
-    scene.addSphere({-2.0, 0.0, -0.3}, 1.0, {{0, 0, 0}, {0.3, 0.8, 0.3}});
-    // scene.addSphere({-1.0, 0.7, -2}, 0.3, {{5, 5, 5}, {0, 0, 0}});
-    scene.addTriangle({0, 1.2, -50}, {-50, 1.0, 50}, {50, 1.0, 50}, {{0, 0, 0}, {0.8, 0.6, 0.6}});
-    scene.addRectangularPrism({3.5, 1, 2}, {3.7, -1, -2.5}, {{0, 0, 0}, {0.6, 0.6, 0.6}});
+    auto const scene = []() {
+        auto s = Scene{{0.9, 0.9, 0.9}};
+        s.addSphere({0, 1, 0}, 1.0, {{0, 0, 0}, {0.9, 0.9, 0.9}});
+        s.addSphere({2.0, 1, -0.3}, 1.0, {{0, 0, 0}, {0.3, 0.3, 0.8}});
+        s.addSphere({-2.0, 1, -0.3}, 1.0, {{0, 0, 0}, {0.3, 0.8, 0.3}});
+        s.addSphere({-1.0, 0.3, -2}, 0.3, {{5, 5, 5}, {0, 0, 0}});
+        s.addRectangularPrism({-1000, 0.0, -1000}, {1000, -0.1, 1000}, {{0, 0, 0}, {0.8, 0.6, 0.6}});
+        s.addRectangularPrism({2.5, 0, -2}, {2.0, 0.5, -1.5}, {{0, 0, 0}, {0.6, 0.6, 0.6}});
+        return s;
+    }();
 
-    auto renderParams = RenderParams{"out.png", 2, 4};
-    auto image = render(scene, camera, renderParams, rng);
+    auto const renderParams = RenderParams{"out.png", 5, 1024};
+    auto const image = render(scene, camera, renderParams, rng);
 
     writePng(renderParams.outputFile, image);
 
