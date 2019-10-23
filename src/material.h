02ad22774ab5eab36774a62c88761e095508fa8d
Tim Quelch <tim@quelch.name>
2019-10-23 11:45:16 +1000

    Add in components for reflectiveness in  material

--- src/material.h
+++ src/material.h
@@ -6,5 +6,7 @@ namespace renderer {
     struct Material {
         Colour emission;
         Colour diffusion;
+        float reflectionIndex = {0.0};
+        float reflectionConeAngle = {0.0};
     };
-}
+} // namespace renderer
